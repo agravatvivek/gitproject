d84bb9d094274984e2ed9d5a6d2fedd97f5af699
vivek <@it.viveka@gmail.com>
2019-04-02 15:11:19 +1300

    Updates

---
+++ precache-manifest.bab891b54d9ffe4a7d48e651955be459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3236326057a7beb77220",
+    "url": "/myapp/static/css/main.8c3c0118.chunk.css"
+  },
+  {
+    "revision": "3236326057a7beb77220",
+    "url": "/myapp/static/js/main.7acc1e1d.chunk.js"
+  },
+  {
+    "revision": "569b2b33e303e431ba73",
+    "url": "/myapp/static/js/runtime~main.778762fe.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/myapp/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a536e2110427256b2dafda9d94d99607",
+    "url": "/myapp/index.html"
+  }
+];
