eda774a1705dc2ba3fa1790c61fbd227adff0f77
vivek <@it.viveka@gmail.com>
2019-04-02 15:28:08 +1300

    Updates

---
+++ precache-manifest.e13265d97a57cf36495af4c6ad602ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "803ea9e3423f3f38424a",
+    "url": "/gitproject/static/css/main.8c3c0118.chunk.css"
+  },
+  {
+    "revision": "803ea9e3423f3f38424a",
+    "url": "/gitproject/static/js/main.a038f8c1.chunk.js"
+  },
+  {
+    "revision": "cb75f885ff20a5281b5a",
+    "url": "/gitproject/static/js/runtime~main.68f1853d.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/gitproject/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitproject/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3357d209f29e1085656f32b5535f9f9",
+    "url": "/gitproject/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.bab891b54d9ffe4a7d48e651955be459.js"
+  "/gitproject/precache-manifest.e13265d97a57cf36495af4c6ad602ca9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/gitproject/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
